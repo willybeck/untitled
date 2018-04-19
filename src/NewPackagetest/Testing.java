eb0cd73103513f110d704b9054d8e3e213d56b1c
rasmuslevinsson <rasmuslewinsson@gmail.com>
2018-04-19 14:53:21 +0200

    rasmus1

--- src/NewPackagetest/Testing.java
+++ src/NewPackagetest/Testing.java
@@ -1,12 +1,18 @@
 package NewPackagetest;
 
 
+import com.sun.javafx.runtime.SystemProperties;
+
 public class Testing {
 
 
     public static void main(String[]args){
+
         System.out.println("hello");
+        System.out.println("rasmus1");
+
     }
+
 
 
 }
