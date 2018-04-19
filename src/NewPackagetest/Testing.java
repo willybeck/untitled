26e3886a5246cb4b04d40ada5e00e2dc3249a8a1
rasmuslevinsson <rasmuslewinsson@gmail.com>
2018-04-19 15:07:20 +0200

    rasmus12

--- src/NewPackagetest/Testing.java
+++ src/NewPackagetest/Testing.java
@@ -6,6 +6,10 @@ public class Testing {
 
     public static void main(String[]args){
         System.out.println("hello");
+        System.out.println("funkar nu?? rasmus2");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
     }
 
 
